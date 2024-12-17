28323bbb184be98f87d420629330fe4ddf6d2b72
Brinda Naveen <brindanaveen0109@gmail.com>
2024-12-17 18:53:03 +0530

    Modified the cli.py to add more clear and concise help

--- task_cli/cli.py
+++ task_cli/cli.py
@@ -9,19 +9,19 @@ def main():
         description = "A CLI tool to manage the tasks."
     )
 
-    subparsers = parser.add_subparsers(dest="command", help="Available commands")
+    subparsers = parser.add_subparsers(dest="command", help="Commands that you an use")
     
-    add_parser = subparsers.add_parser("add", help="Add a new task")
+    add_parser = subparsers.add_parser("add", help="Add a new task to your list by passing arguments in the cli : add <task_title>")
     add_parser.add_argument("--description", "-d", required=True, help="Description of the task")
     
-    list_parser = subparsers.add_parser("list", help="list all tasks")
+    list_parser = subparsers.add_parser("list", help="list all tasks or list tasks according to the status by passing arguments in the cli : list | list-done | list-not-done | list-in-progress")
     list_parser.add_argument("--status", "-s", choices=["not-done", "done", "in-progress"], help="Filter task by status")
 
-    update_parser = subparsers.add_parser("update", help="Update the satus")
+    update_parser = subparsers.add_parser("update", help="Update the status of a task by passing argument in the command line with options : update --id <task-id> --status <task-status>")
     update_parser.add_argument("--id", "-i", type=int, required=True, help="Update the status of a task by ID")
     update_parser.add_argument("--status", "-s", choices=["not-done", "done", "in-progress"], required=True, help="New status of the task")
 
-    delete_parser = subparsers.add_parser("delete", help="Delete a task")
+    delete_parser = subparsers.add_parser("delete", help="Delete a task by passing arguments in the command line: delete --id <task-id>")
     delete_parser.add_argument("--id", "-i", type=int, required=True, help="Task ID to delete")
 
     args = parser.parse_args()
